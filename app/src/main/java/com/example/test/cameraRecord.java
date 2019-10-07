cd49693662a166ac454f95ae2626beb0d8e2f2c0
Aditya Vikram Sharma <45156422+adityasharma1796@users.noreply.github.com>
2019-10-07 16:34:41 -0700

    Incorporate camera module

--- app/src/main/java/com/example/test/cameraRecord.java
+++ app/src/main/java/com/example/test/cameraRecord.java
@@ -1,6 +1,13 @@
 package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 
 import android.os.Bundle;
 
@@ -10,5 +17,13 @@ public class cameraRecord extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_record);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA},
+                    0);
+        }
+        Intent intent = new Intent("android.media.action.VIDEO_CAPTURE");
+        startActivity(intent);
     }
 }
